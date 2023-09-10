88adcc29fbc206054b2c216496c9f2c0c5c2577e
Pat O'Connor <patoconnor43@gmail.com>
2023-09-09 20:19:18 -0700

    Make changes to support adding tags to statuses

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,7 +2,7 @@
 now=`date "+%s"`
 one_day_ago=$((now - 86400))
 
-item_json=`cat public/index.xml | hq '{item: item | {title: title, pubDate: pubDate, guid: guid}}'`
+item_json=`cat public/index.xml | hq '{item: item | {title: title, pubDate: pubDate, guid: guid, description: description}}'`
 pubDate=`echo $item_json | jq -r '.item.pubDate'`
 
 platform=`uname | tr '[:upper:]' '[:lower:]'`
@@ -23,13 +23,18 @@ if [ $diff_unix -ge 0 ]; then
 
     title=`echo $item_json | jq -r '.item.title' | tr '"' "'"`
     link=`echo $item_json | jq -r '.item.guid'`
+    tags=`echo $item_json | jq -r '.item.description' | gsed -nr 's/.*::: (.*) :::.*/\1/p'`
+    tag_string=""
+    if [ "$tags" != "" ]; then
+        tag_string=`echo $tags | gsed -r 's/(\w+)/\#\1/g'`
+    fi
 
-    echo "Posting a toot with the title: $title and link: $link"
+    echo "Posting a toot with the title: $title link: $link tag_string: $tag_string"
 
     curl -X POST -H "Authorization: Bearer $token" -H "Idempotency-Key: $title" "$MASTODON_DOMAIN/api/v1/statuses" \
         -F "status=$title
-
 $link
+$tag_string
 " \
         -F "visibility=$visibility" | jq '.'
 
